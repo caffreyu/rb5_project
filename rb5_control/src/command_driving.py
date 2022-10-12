f3ffb8f9af92d27f0b010550185f53fb0ea5ed95
caffreyu <caffreyu@gmail.com>
2022-10-12 16:45:20 -0700

    wip: added command_driving class, need clean up for python2

---
+++ rb5_control/src/command_driving.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python
+import rospy
+from sensor_msgs.msg import Joy
+from copy import deepcopy
+from typing import List
+from math import sqrt, atan2
+
+joy_msg = Joy()
+joy_msg.axes = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+joy_msg.buttons = [0, 0, 0, 0, 0, 0, 0, 0]
+
+speed_x, speed_y, speed_theta_cw, speed_theta_ccw = []
+command_x, command_y, command_theta = []
+
+class CommandDriving:
+    """Class that follows the giving waypoint command."""
+    
+    def __init__(
+        self, 
+        current_state : List[float],
+        command_state : List[float] = [0.0, 0.0, 0.0],
+    ):
+        """
+        Initialize the CommandDriving class.
+        
+        Input:
+            current_state: current state of the robot.
+            command_state: command waypoint for the robot. 
+        """
+        assert len(current_state) == 3, \
+            "Length of current_state is off"
+        assert len(command_state) == 3, \
+            "Length of command_state is off"
+        self.current_state, self.command_state = current_state, command_state   
+        self.current_x, self.current_y, self.current_theta = current_state
+        self.command_x, self.command_y, self.command_theta = command_state
+        self.pub = rospy.Publisher('/cmd', Joy, queue_size = 5)
+        
+        self.pub.publish(joy_msg)
+        rospy.sleep(0.5)
+    
+    def move(self, distance : float):
+        """Move the robot in forward / reverse direction."""
+        rospy.loginfo("Moving the robot")
+        delta_time = abs(distance) / speed_x
+        current_msg = deepcopy(joy_msg)
+        current_msg.axes[1] = -command_x * distance / abs(distance)
+        rospy.sleep(delta_time)
+    
+    def slide(self, distance : float):
+        """Slide the robot in right / left direction."""
+        rospy.loginfo("Sliding the robot")
+        delta_time = abs(distance) / speed_y
+        current_msg = deepcopy(joy_msg)
+        current_msg.axes[0] = -command_y * distance / abs(distance)
+        rospy.sleep(delta_time)
+    
+    def rotate(self, angle : float):
+        """Rotate the robot in clockwise / counter clockwise direction."""
+        rospy.loginfo("Rotating the robot")
+        if angle < 0: delta_time = abs(angle) / speed_theta_ccw
+        else: delta_time = abs(angle) / speed_theta_cw
+        current_msg = deepcopy(joy_msg)
+        current_msg.axes[2] = -command_theta * angle / abs(angle)
+        rospy.sleep(delta_time)
+    
+    def run(self):
+        """Move the robot from current state to command state."""
+        delta_x, delta_y, delta_theta = \
+            [ele[0] - ele[1] 
+             for ele in zip(self.command_state, self.current_state)]
+        if self.current_theta % 3.14 == 0:
+            # robot facing x / -x direction
+            # x directional movement will be moving
+            # y directional movement will be sliding
+            self.move(delta_x)
+            self.slide(delta_y)
+        elif self.current_theta % 1.57 == 0:
+            # robot facing y / -y direction
+            # x directional movement will be sliding
+            # y directional movement will be moving
+            self.move(delta_y)
+            self.move(delta_x)
+        else:
+            # rotate the robot to the desired direction first
+            # then move to the command position
+            current_cmd_angle = atan2(delta_y, delta_x)
+            angle = current_cmd_angle - self.current_theta
+            delta_theta = current_cmd_angle - self.command_theta
+            distance = sqrt(pow(delta_x, 2) + pow(delta_y, 2))
+            self.rotate(angle)
+            self.move(distance)
+        
+        self.rotate(delta_theta)
+        self.pub.publish(joy_msg)
+        rospy.sleep(1.0)
+        
---
+++ rb5_control/src/waypoints_following.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+from command_driving import CommandDriving
+
+# processing waypoints
+with open('/root/ros_ws/src/rb5_project/rb5_control/src/waypoints.txt') as f:
+    waypoints_file = f.readlines()
+
+waypoints_string_ls = [ele[: -1].split(',') for ele in waypoints_file]
+waypoints_string_ls[-1][-1] = '0'
+waypoints_ls = []
+
+for string_ls in waypoints_string_ls:
+    waypoints_ls.append([float(ele) for ele in string_ls])
+
+waypoints_ls = waypoints_ls[: 3]
+
+current_state = [0.0, 0.0, 0.0]
+
+while waypoints_ls:
+    command_driver = \
+        CommandDriving(
+            current_state=current_state,
+            command_state=command_state,
+        )
+    command_driver.run()
+    
