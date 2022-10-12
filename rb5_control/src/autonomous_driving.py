0a1798521f12ef2f687c5ec6a03c20fe722e69c2
caffreyu <caffreyu@gmail.com>
2022-10-12 07:53:45 +0000

    wip: need calibration and modify the autonomous_driving script

--- rb5_control/src/autonomous_driving.py
+++ rb5_control/src/autonomous_driving.py
@@ -7,28 +7,34 @@ from std_msgs.msg import Int8
 from math import atan2
 
 # processing waypoints
-with open('/home/caffreyu/FA22/cse276a/src/rb5_ros/rb5_control/src/waypoints.txt') as f:
+with open('/root/ros_ws/src/rb5_project/rb5_control/src/waypoints.txt') as f:
     waypoints_file = f.readlines()
 
 waypoints_string_ls = [ele[: -1].split(',') for ele in waypoints_file]
+waypoints_string_ls[-1][-1] = '0'
 waypoints_ls = []
 
 for string_ls in waypoints_string_ls:
     waypoints_ls.append([float(ele) for ele in string_ls])
 
-speed_ls = [0.5, 0.5, 0.5]
-x_speed, y_speed, theta_speed = speed_ls
+waypoints_ls = waypoints_ls[: 3]
 
-speed_cmd = [0.5, 0.5, 0.25]
+speed_ls = [0.35, 0.185, 1.4, 1.3]
+x_speed, y_speed, theta_speed_ccw, theta_speed_cw = speed_ls
+
+speed_cmd = [1.2, 0.5, 1.0]
 x_cmd, y_cmd, theta_cmd = speed_cmd
 
 rospy.init_node("auto_driving")
 pub_auto = rospy.Publisher("/auto", Joy, queue_size = 1)
-current_state = [0, 0, 0]
+current_state = [0, 0, 1.57]
 
 joy_msg = Joy()
 joy_msg.axes = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
 joy_msg.buttons = [0, 0, 0, 0, 0, 0, 0, 0]
+
+pub_auto.publish(joy_msg)
+rospy.sleep(1.0)
 
 def move_y(delta_y):
     delta_time = abs(delta_y) / y_speed
@@ -36,52 +42,66 @@ def move_y(delta_y):
     direction = 1 if delta_y > 0 else -1
     curr_msg.axes[1] = direction * y_cmd
     pub_auto.publish(curr_msg)
-    rospy.loginfo(f"sleeping for {delta_time}")
+    print (delta_time)
     rospy.sleep(delta_time)
 
 def move_x(delta_x):
+    rospy.loginfo("in move x")
     delta_time = abs(delta_x) / x_speed
     curr_msg = deepcopy(joy_msg)
     direction = 1 if delta_x > 0 else -1
-    curr_msg.axes[0] = direction * x_cmd
+    curr_msg.axes[0] = -direction * x_cmd
+    rospy.loginfo(delta_time)
     pub_auto.publish(curr_msg)
-    rospy.loginfo(f"sleeping for {delta_time}")
+    print (delta_time)
     rospy.sleep(delta_time)
 
 def move_theta(delta_theta):
-    delta_time = abs(delta_theta) / theta_speed
     curr_msg = deepcopy(joy_msg)
-    direction = 1 if delta_theta > 0 else -1
-    curr_msg.axes[2] = direction * theta_cmd
+    if delta_theta > 0:
+        delta_time = abs(delta_theta) / theta_speed_ccw
+        curr_msg.axes[2] = theta_cmd
+    else:
+        delta_time = abs(delta_theta) / theta_speed_cw
+        curr_msg.axes[2] = -theta_cmd
+    # direction = 1 if delta_theta > 0 else -1
+    # curr_msg.axes[2] = direction * theta_cmd
     pub_auto.publish(curr_msg)
-    rospy.loginfo(f"sleeping for {delta_time}")
+    print (delta_time)
     rospy.sleep(delta_time)
 
 while waypoints_ls:
 
-    data = rospy.wait_for_message("/auto_stop", Int8)
-    if data.data != 1: break
+    # data = rospy.wait_for_message("/auto_stop", Int8)
+    # if data.data != 1: break
 
     w_x, w_y, w_theta = waypoints_ls.pop(0)
+
+    print ([w_x, w_y, w_theta])
+    w_x = w_x / 2
+    w_y = w_y / 2
     if w_theta < 0:
         w_theta = -w_theta + 3.14
     c_x, c_y, c_theta = current_state
     delta_x, delta_y, delta_theta = w_x - c_x, w_y - c_y, w_theta - c_theta
+
+    print "delta_x " + str(delta_x) + " delta_y " + str(delta_y) + " delta_theta " + str(delta_theta)
     
     if delta_theta == delta_y == delta_x == 0:
         rospy.loginfo("passed")
-        continue
+        # current_state = [w_x, w_y, w_theta]
     # slide
     if delta_theta == delta_y == 0 and delta_x != 0:
         rospy.loginfo("slide")
         move_x(delta_x)
-        continue
+        # current_state = [w_x, w_y, w_theta]
     # move in single y direction:
     elif delta_x == 0:
         rospy.loginfo("head in y")
         move_y(delta_y)
     else:
         moving_direction = atan2(delta_y, delta_x)
+        print ("moving direction is ", moving_direction)
         if moving_direction < 0: moving_direction += 2 * 3.14
         delta_moving_direction = moving_direction - c_theta
         rospy.loginfo("head to moving direction")
@@ -92,9 +112,11 @@ while waypoints_ls:
         move_y(moving_distance)
     
     rospy.loginfo("adjust direction")
-    move_theta(delta_theta)
+    if delta_theta != 0: move_theta(delta_theta)
     current_state = [w_x, w_y, w_theta]
 
+
+    pub_auto.publish(joy_msg)
+    rospy.sleep(1.0)
+
 pub_auto.publish(joy_msg)
-
-
